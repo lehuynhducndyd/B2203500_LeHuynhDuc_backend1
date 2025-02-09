5de9167e983d9e126fc7a7c817f45feb3f1145bf
Le Huynh Duc <lehuynhducndyd@gmail.com>
2025-02-09 12:21:09 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const contactsRouters = require("./app/routes/contact.route");
+
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts",contactsRouters)
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
